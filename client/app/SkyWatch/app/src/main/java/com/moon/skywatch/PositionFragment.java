7f7f19a753c8cb11c7ed5c907bc2e7cfed35de76
hoon <mjhdevil@naver.com>
2022-12-05 16:47:25 +0900

    1205 - 16:47 moon

--- client/app/SkyWatch/app/src/main/java/com/moon/skywatch/PositionFragment.java
+++ client/app/SkyWatch/app/src/main/java/com/moon/skywatch/PositionFragment.java
@@ -1,6 +1,7 @@
 package com.moon.skywatch;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.content.pm.ActivityInfo;
@@ -9,17 +10,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.moon.skywatch.databinding.FragmentPositionBinding;
 
-public class PositionFragment extends Fragment{
+public class PositionFragment extends Fragment implements OnMapReadyCallback {
 
     private FragmentPositionBinding binding;
+    private MapView mapView;
+    private View view;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        binding = FragmentPositionBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        view = inflater.inflate(R.layout.fragment_position, container, false);
 
         getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
@@ -28,13 +39,51 @@ public class PositionFragment extends Fragment{
 //        ViewGroup mapViewContainer = (ViewGroup) viewGroup.findViewById(R.id.kakao_map);
 //        mapViewContainer.addView(mapView);
 
+        mapView = (MapView)view.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
 
-        return root;
+
+
+        return view;
+    }
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng marker = new LatLng(37.568291,126.997780);
+        googleMap.addMarker(new MarkerOptions().position(marker).title("여기"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(marker));
+
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+    @Override
+    public void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
     }
 }
