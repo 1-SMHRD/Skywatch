906cdb71ee2767a66e25b5c86ff754ef4aceb8b9
hoon <mjhdevil@naver.com>
2022-12-06 15:02:30 +0900

    1206 - 15:02 moon

--- client/app/SkyWatch/app/src/main/java/com/moon/skywatch/PositionFragment.java
+++ client/app/SkyWatch/app/src/main/java/com/moon/skywatch/PositionFragment.java
@@ -1,7 +1,6 @@
 package com.moon.skywatch;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.content.pm.ActivityInfo;
@@ -10,80 +9,76 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.moon.skywatch.databinding.FragmentPositionBinding;
 
-public class PositionFragment extends Fragment implements OnMapReadyCallback {
+public class PositionFragment extends Fragment implements OnMapReadyCallback{
 
     private FragmentPositionBinding binding;
-    private MapView mapView;
     private View view;
+    private GoogleMap mMap;
+    private MapView mMapView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.fragment_position, container, false);
-
         getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        //        MapView mapView = new MapView(getContext());
-//
-//        ViewGroup mapViewContainer = (ViewGroup) viewGroup.findViewById(R.id.kakao_map);
-//        mapViewContainer.addView(mapView);
 
-        mapView = (MapView)view.findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(this);
+        mMapView = (MapView) view.findViewById(R.id.mapView2);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull GoogleMap googleMap) {
+                mMap = googleMap;
 
+                LatLng latLng = new LatLng(35.149796202004325, 126.91992834014);
+                mMap.addMarker(new MarkerOptions()
+                        .position(latLng)
+                        .title("스마트인재개발원"));
+//                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,16));
+            }
+        });
 
 
         return view;
     }
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        LatLng marker = new LatLng(37.568291,126.997780);
-        googleMap.addMarker(new MarkerOptions().position(marker).title("여기"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(marker));
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
+//        LatLng marker = new LatLng(37.568291,126.997780);
+//        googleMap.addMarker(new MarkerOptions().position(marker).title("여기"));
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(marker));
 
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        mapView.onStart();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        mapView.onStop();
     }
     @Override
     public void onResume() {
         super.onResume();
-        mapView.onResume();
+        mMapView.onResume();
     }
     @Override
     public void onPause() {
         super.onPause();
-        mapView.onPause();
+        mMapView.onPause();
     }
     @Override
     public void onLowMemory() {
         super.onLowMemory();
-        mapView.onLowMemory();
+        mMapView.onLowMemory();
     }
     @Override
     public void onDestroy() {
-        mapView.onDestroy();
+        mMapView.onDestroy();
         super.onDestroy();
     }
 }
