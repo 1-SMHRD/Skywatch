a4ecb26a10871e5882e4ed40cd6865cc916fa8e8
bob-sin <109669971+bob-sin@users.noreply.github.com>
2022-12-13 16:02:59 +0900

    adasf

--- server/views/features.py
+++ server/views/features.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, redirect, flash, Flask,url_for,request,render_template, Response
-from module import dbmodule
+from module import dbmodule, Control_auto
 from socket import *
 import time
 import os
@@ -107,6 +107,10 @@ def getImgDir():
     
     msg = "success"
     
+    video_stream()
+    Control_auto.drone_control.move_A(video_camera)
+
+
     return msg
 
 """
